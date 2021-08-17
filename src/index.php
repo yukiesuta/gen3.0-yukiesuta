bcd7229a903a572980cd109372a8df821d92c905
Yuya Makiyama <mackyym120307@gmail.com>
2021-08-17 13:56:57 +0900

    8/17 progress report

--- src/index.php
+++ src/index.php
@@ -1,11 +1,10 @@
 <?php
-require(dirname(__FILE__) . "/dbconnect.php");
+require('dbconnect.php');
 
-$stmt = $db->query('SELECT events.name, events.start_at, events.end_at, count(event_attendance.id) AS total_participants FROM events LEFT JOIN event_attendance ON events.id = event_attendance.event_id GROUP BY events.id');
+$stmt = $db->query('SELECT events.id, events.name, events.start_at, events.end_at, count(event_attendance.id) AS total_participants FROM events LEFT JOIN event_attendance ON events.id = event_attendance.event_id GROUP BY events.id');
 $events = $stmt->fetchAll();
 
-function get_day_of_week($w)
-{
+function get_day_of_week ($w) {
   $day_of_week_list = ['日', '月', '火', '水', '木', '金', '土'];
   return $day_of_week_list["$w"];
 }
@@ -24,18 +23,18 @@ function get_day_of_week($w)
 
 <body>
   <header class="h-16">
-    <div class="flex justify-between items-center w-96 h-full mx-auto pl-2 pr-5">
+    <div class="flex justify-between items-center w-full h-full mx-auto pl-2 pr-5">
       <div class="h-full">
         <img src="img/header-logo.png" alt="" class="h-full">
       </div>
       <div>
-        <a href="/auth/login" class="text-white bg-blue-400 px-4 py-2 rounded-3xl">ログイン</a>
+        <a href="/auth/login" class="text-white bg-blue-400 px-4 py-2 rounded-3xl bg-gradient-to-r from-blue-600 to-blue-200">ログイン</a>
       </div>
     </div>
   </header>
 
   <main class="bg-gray-100">
-    <div class="w-96 mx-auto p-5">
+    <div class="w-full mx-auto p-5">
       <div id="filter" class="mb-8">
         <h2 class="text-sm font-bold mb-3">フィルター</h2>
         <div class="flex">
@@ -60,7 +59,7 @@ function get_day_of_week($w)
           $end_date = strtotime($event['end_at']);
           $day_of_week = get_day_of_week(date("w", $start_date));
           ?>
-          <div class="bg-white mb-3 p-4 flex justify-between rounded-md shadow-md">
+          <div class="modal-open bg-white mb-3 p-4 flex justify-between rounded-md shadow-md cursor-pointer" id="event-<?php echo $event['id']; ?>">
             <div>
               <h3 class="font-bold text-lg mb-2"><?php echo $event['name'] ?></h3>
               <p><?php echo date("Y年m月d日（${day_of_week}）", $start_date); ?></p>
@@ -70,8 +69,14 @@ function get_day_of_week($w)
             </div>
             <div class="flex flex-col justify-between text-right">
               <div>
-                <p class="text-sm">未回答</p>
-                <p class="text-xs">期限 <?php echo date("m月d日", strtotime('-3 day', $end_date)); ?></p>
+                <?php if ($event['id'] % 3 === 1) : ?>
+                  <p class="text-sm font-bold text-yellow-400">未回答</p>
+                  <p class="text-xs text-yellow-400">期限 <?php echo date("m月d日", strtotime('-3 day', $end_date)); ?></p>
+                <?php elseif ($event['id'] % 3 === 2) : ?>
+                  <p class="text-sm font-bold text-gray-300">不参加</p>
+                <?php else : ?>
+                  <p class="text-sm font-bold text-green-400">参加</p>
+                <?php endif; ?>
               </div>
               <p class="text-sm"><span class="text-xl"><?php echo $event['total_participants']; ?></span>人参加 ></p>
             </div>
@@ -80,6 +85,25 @@ function get_day_of_week($w)
       </div>
     </div>
   </main>
+
+  <div class="modal opacity-0 pointer-events-none fixed w-full h-full top-0 left-0 flex items-center justify-center">
+    <div class="modal-overlay absolute w-full h-full bg-black opacity-80"></div>
+
+    <div class="modal-container absolute bottom-0 bg-white w-screen h-4/5 rounded-t-3xl shadow-lg z-50">
+      <div class="modal-content text-left py-6 pl-10 pr-6">
+        <div class="z-50 text-right mb-5">
+          <svg class="modal-close cursor-pointer inline bg-gray-100 p-1 rounded-full" xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 18 18">
+            <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
+          </svg>
+        </div>
+
+        <div id="modalInner"></div>
+
+      </div>
+    </div>
+  </div>
+
+  <script src="/js/main.js"></script>
 </body>
 
 </html>
