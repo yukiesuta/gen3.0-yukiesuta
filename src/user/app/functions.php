57c93f0704b9c691bb95eb2750c0e318ed81d082
“yukiesuta” <matsumoto.yukiho.0704@gmail.com>
2022-04-21 17:27:09 +0900

    微妙な修正

--- src/user/app/functions.php
+++ src/user/app/functions.php
@@ -8,21 +8,21 @@ function h($str)
 function getPdoInstance(){
     try {
         $pdo = new PDO(
-          DSN,
-          DB_USER,
-          DB_PASS,
-          [
+        DSN,
+        DB_USER,
+        DB_PASS,
+        [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
             PDO::ATTR_EMULATE_PREPARES => false,
-          ]
+        ]
         );
 
         return $pdo;
-      } catch (PDOException $e) {
+    } catch (PDOException $e) {
         echo $e->getMessage();
         exit;
-      }
+    }
 }
 
 function get_agency_informations($pdo)
