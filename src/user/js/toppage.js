5c18d499c33c72486288c36361fbd59d2f774951
“yukiesuta” <matsumoto.yukiho.0704@gmail.com>
2022-05-10 21:18:38 +0900

    idが取れない場合は読み込まない

--- src/user/js/toppage.js
+++ src/user/js/toppage.js
@@ -1,24 +1,20 @@
 'use strict';
 
-var flag = false;
+for (let i = 0; i < 100; i++) {
+    if (document.getElementById('flexCheckDefault' + i)) {
+        let checkbox = document.getElementById('flexCheckDefault' + i)
+        let rightContent = document.getElementById('rightContent' + i)
+        checkbox.addEventListener('change', function() {
+            if (checkbox.checked == true) {
 
+                console.log('選択されました')
 
-for (let i = 1; i < 3; i++) {
-    let checkbox = document.getElementById('flexCheckDefault' + i)
-    let rightContent = document.getElementById('rightContent' + i)
-    checkbox.addEventListener('change',function(){
-        if (checkbox.checked == true) {
+                rightContent.classList.remove('display-none')
+            } else if (checkbox.checked == false) {
+                console.log('解除されました')
 
-            console.log('選択されました')
-
-            rightContent.classList.remove('display-none')
-        }
-        else if (checkbox.checked == false){
-            console.log('解除されました')
-
-            rightContent.classList.add('display-none')
-        }
+                rightContent.classList.add('display-none')
+            }
+        }, false)
     }
-    ,false
-    )
 }
