68e3cf78612ff1710366885880d33091b3b705e7
Kensuke Morimoto <morimotokensuke0104@gmail.com>
2022-05-11 10:21:53 +0900

    responsive header toppage

--- src/user/html/toppage.php
+++ src/user/html/toppage.php
@@ -39,8 +39,40 @@ if (isset($_POST["sort_change"])) {
         <div class="headerTitle text-center fs-2">
             Craft
         </div>
-
-        <div class="navigations mt-4">
+        <nav class="navbar navbar-expand-lg navbar-dark">
+            <a class="navbar-brand ms-3" href="#">Craft</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-center" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item active me-5">
+                        <a class="nav-link" href="#">就活サイト</a>
+                    </li>
+                    <li class="nav-item me-5">
+                        <a class="nav-link" href="#">就活支援サービス</a>
+                    </li>
+                    <li class="nav-item me-5">
+                        <a class="nav-link" href="#">自己分析診断ツール</a>
+                    </li>
+                    <li class="nav-item me-5">
+                        <a class="nav-link" href="#">ES添削サービス</a>
+                    </li>
+                    <li class="nav-item me-5">
+                        <a class="nav-link" href="#">就活エージェント</a>
+                    </li>
+                    <li class="nav-item me-5">
+                        <a class="nav-link" href="#">就職の教科書とは</a>
+                    </li>
+                    <li class="nav-item me-5">
+                        <a class="nav-link" href="#">お問い合わせ</a>
+                    </li>
+                    
+                </ul>
+            </div>
+        </nav>
+        
+        <!-- <div class="navigations mt-4">
             <ul class="d-flex justify-content-center mb-0">
                 <a href="" class="navigation me-5">
                     <li>就活サイト</li>
@@ -64,7 +96,7 @@ if (isset($_POST["sort_change"])) {
                     <li>お問い合わせ</li>
                 </a>
             </ul>
-        </div>
+        </div> -->
     </header>
     <main>
         <div class="first-view">
@@ -252,7 +284,9 @@ if (isset($_POST["sort_change"])) {
         </div>
     </main>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="../js/toppage.js"></script>
 </body>
 
