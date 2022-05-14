1235b42cda01dd614b31a24d66409ef5f8e4c986
“yukiesuta” <matsumoto.yukiho.0704@gmail.com>
2022-05-14 23:23:40 +0900

    js一部編集

--- src/user/html/toppage.php
+++ src/user/html/toppage.php
@@ -292,21 +292,6 @@ if (isset($_POST["sort_change"])) {
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="../js/toppage.js"></script>
-    <script>
-    function checked_check(){
-
-        arr = new Array()
-
-        <?php foreach ($agency_informations as $agency_information) : ?>
-            if(document.getElementById("flexCheckDefault<?= h($agency_information->id); ?>").checked){
-                arr.push('<?= h($agency_information->id); ?>');
-            }
-        <?php endforeach; ?>
-        console.log(arr);
-        let json = JSON.stringify(arr);
-        localStorage.setItem('keyname', arr);
-    }
-    </script>
 </body>
 
 </html>
