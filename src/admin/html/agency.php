e8e7413d2ab34f928e8be0b831c4738b6c5b5fea
“yukiesuta” <matsumoto.yukiho.0704@gmail.com>
2022-05-19 21:08:40 +0900

    名前分かりづらいよな、、、

--- src/admin/html/agency.php
+++ src/admin/html/agency.php
@@ -40,7 +40,7 @@ $inquiry_results = $inquiry_stmt->fetchAll();
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/agency.css">
-    <title>Document</title>
+    <title>各エージェンシーのページ</title>
 </head>
 
 <body>
--- src/admin/html/boozer-login.php
+++ src/admin/html/boozer-login.php
@@ -31,7 +31,7 @@ if (!empty($_POST)) {
   <link rel="stylesheet" href="../css/normalize.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <link rel="stylesheet" href="../css/boozer-login.css">
-  <title>ログイン</title>
+  <title>ブーザーのログイン</title>
 </head>
 
 <body>
