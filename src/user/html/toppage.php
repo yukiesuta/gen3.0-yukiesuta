d879a16be22b279aaacbc624bbfe1b211a07da66
“yukiesuta” <matsumoto.yukiho.0704@gmail.com>
2022-05-28 02:48:57 +0900

    変に全部送信されちゃうの治した

--- src/user/html/toppage.php
+++ src/user/html/toppage.php
@@ -503,7 +503,7 @@ $agency_feature_comparison = $stmt->fetchAll();
                         
                     </div>
                     <?php foreach ($agency_informations as $agency_information) : ?>
-                        <input class="form-check-input" type="hidden" value="" name="agency<?= h($agency_information->agency_id); ?>" id="hidden_checkbox<?= h($agency_information->agency_id); ?>">
+                        <input class="form-check-input d-none" type="checkbox" value="" name="agency<?= h($agency_information->agency_id); ?>" id="hidden_checkbox<?= h($agency_information->agency_id); ?>">
                     <?php endforeach; ?>
                     <div class="submit" id="submit-button">
                         <button type="submit" value="確認画面へ" id="form-button" class="btn btn-success mt-5 mb-5">
