2fd9c07aad44a590f2492cab5443208a8da58829
“yukiesuta” <matsumoto.yukiho.0704@gmail.com>
2022-05-29 20:33:31 +0900

    編集完了ページ

--- src/admin/html/boozer-edit-creation.php
+++ src/admin/html/boozer-edit-creation.php
@@ -43,7 +43,7 @@ $result = $stmt->fetch();
         Craft
     </header>
     <main>
-        <form class=" p-5 ms-5 me-5 form"  method="post" id="inquiry" enctype="multipart/form-data">
+        <form class=" p-5 ms-5 me-5 form"  method="post" id="inquiry" enctype="multipart/form-data" action="boozer-edit-complete.php?id=<?=$id?>">
             <div class=" mb-3 ms-5 me-5 text-center title">
                 編集フォーム
             </div>
