78300305b2a9dee742e378d03a51b596e17803c2
Sohtatsu Ido <sohtatsu.ido@keio.jp>
2022-09-06 23:37:24 +0900

    テーブル調整、中間テーブル作成

--- mysql/sql/init.sql
+++ mysql/sql/init.sql
@@ -13,16 +13,30 @@ CREATE TABLE events (
   deleted_at DATETIME
 );
 
-DROP TABLE IF EXISTS event_attendance;
-CREATE TABLE event_attendance (
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
   id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-  event_id INT NOT NULL,
-  user_id INT,
+  email VARCHAR(128) NOT NULL,
+  name VARCHAR(20) NOT NULL,
+  password VARCHAR(128) NOT NULL,
+  is_admin BOOLEAN,
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
   updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   deleted_at DATETIME
 );
 
+DROP TABLE IF EXISTS event_user_attendance;
+CREATE TABLE event_user_attendance (
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  event_id INT NOT NULL,
+  user_id INT NOT NULL,
+  attendance_status INT DEFAULT 0,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  deleted_at DATETIME,
+  FOREIGN KEY (event_id) REFERENCES events(id),
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
 
 INSERT INTO events SET name='縦モク', start_at='2021/08/01 21:00', end_at='2021/08/01 23:00';
 INSERT INTO events SET name='横モク', start_at='2021/08/02 21:00', end_at='2021/08/02 23:00';
@@ -40,9 +54,15 @@ INSERT INTO events SET name='遊び', start_at='2021/09/22 18:00', end_at='2021/
 INSERT INTO events SET name='ハッカソン', start_at='2021/09/03 10:00', end_at='2021/09/03 22:00';
 INSERT INTO events SET name='遊び', start_at='2021/09/06 18:00', end_at='2021/09/06 22:00';
 
-INSERT INTO event_attendance SET event_id=1;
-INSERT INTO event_attendance SET event_id=1;
-INSERT INTO event_attendance SET event_id=1;
-INSERT INTO event_attendance SET event_id=2;
-INSERT INTO event_attendance SET event_id=2;
-INSERT INTO event_attendance SET event_id=3;
+INSERT INTO users (email, name, password, is_admin) VALUES
+    ("user0@gmail.com", "user0", "password0", true),
+    ("user1@gmail.com", "user1", "password1", false),
+    ("user2@gmail.com", "user2", "password2", false),
+    ("user3@gmail.com", "user3", "password3", false);
+    
+INSERT INTO event_user_attendance SET event_id=1, user_id=1;
+INSERT INTO event_user_attendance SET event_id=1, user_id=1;
+INSERT INTO event_user_attendance SET event_id=1, user_id=1;
+INSERT INTO event_user_attendance SET event_id=2, user_id=2;
+INSERT INTO event_user_attendance SET event_id=2, user_id=2;
+INSERT INTO event_user_attendance SET event_id=3, user_id=3;
