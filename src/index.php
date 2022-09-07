c2e3808a5131ee505147360fbf9f0d12990e1dec
Sohtatsu Ido <sohtatsu.ido@keio.jp>
2022-09-07 09:48:16 +0900

    テーブル変更に関連したsql文変更

--- src/index.php
+++ src/index.php
@@ -1,7 +1,7 @@
 <?php
 require('dbconnect.php');
 
-$stmt = $db->query('SELECT events.id, events.name, events.start_at, events.end_at, count(event_attendance.id) AS total_participants FROM events LEFT JOIN event_attendance ON events.id = event_attendance.event_id GROUP BY events.id');
+$stmt = $db->query('SELECT events.id, events.name, events.start_at, events.end_at, count(event_user_attendance.id) AS total_participants FROM events LEFT JOIN event_user_attendance ON events.id = event_user_attendance.event_id GROUP BY events.id');
 $events = $stmt->fetchAll();
 
 function get_day_of_week ($w) {
