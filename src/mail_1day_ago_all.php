76722ff0f422c44df2e8d320db1a2058f7af16cb
“yukiesuta” <matsumoto.yukiho.0704@gmail.com>
2022-09-08 09:56:54 +0900

    内容が微妙に違ったから修正

--- src/mail_1day_ago_all.php
+++ src/mail_1day_ago_all.php
@@ -26,7 +26,7 @@ foreach ($events as $event) {
         foreach ($users as $user) {
             $to = $user['email'];
             $subject = "一日前通知！";
-            $body = '【リマインド】イベント名「' . $event['name'] . '」の一日前です。内容は「' .$event['detail']  .'」です。開催日時は' .$event['start_at']  .'です。回答をお願いします。このメールは入力状況に関わらず全員に通知しています。';
+            $body = '【リマインド】イベント名「' . $event['name'] . '」の一日前です。内容は「' .$event['detail']  .'」です。開催日時は' .$event['start_at']  .'です。このメールは入力状況に関わらず全員に通知しています。';
             $headers = "From: system@posse-ap.com";
             $headers .= "MIME-Version: 1.0\n";
             $headers .= "Content-Transfer-Encoding: BASE64\n";
