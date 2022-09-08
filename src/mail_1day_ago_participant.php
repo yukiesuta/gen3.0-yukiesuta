2742189f4939764a39f89e6d4a1fbfef337f3e0c
“yukiesuta” <matsumoto.yukiho.0704@gmail.com>
2022-09-08 10:05:38 +0900

    内容が微妙に間違っていたため微修正

--- src/mail_1day_ago_participant.php
+++ src/mail_1day_ago_participant.php
@@ -6,8 +6,8 @@ $events = $stmt->fetchAll();
 
 foreach ($events as $event) {
     $to = $event['email'];
-    $subject = "未回答者様へ";
-    $body = '【リマインド】イベント名「' . $event['name'] . '」の一日前です。内容は「' . $event['detail']  . '」です。開催日時は' . $event['start_at']  . 'です。回答をお願いします。このメールは参加者のみに通知しています。';;
+    $subject = "参加者様へ";
+    $body = '【リマインド】イベント名「' . $event['name'] . '」の一日前です。内容は「' . $event['detail']  . '」です。開催日時は' . $event['start_at']  . 'です。このメールは参加者のみに通知しています。';;
     $headers = "From: system@posse-ap.com";
     $headers .= "MIME-Version: 1.0\n";
     $headers .= "Content-Transfer-Encoding: BASE64\n";
