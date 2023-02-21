fc1402011f97f06cf520922d64135c17f4770344
asaka0408 <sakanadaisuki0408@softbank.ne.jp>
2023-02-21 22:24:15 +0900

    アカウントダミーデータ追加

--- src/database/seeds/UserTableSeeder.php
+++ src/database/seeds/UserTableSeeder.php
@@ -22,15 +22,43 @@ class UserTableSeeder extends Seeder
             "role_id"      => Role::getAdminId(),
         ]);
         User::create([
-            "name"         => '一般ユーザー',
-            "email"        => 'user@gmail.com',
+            "name"         => '料理クラブ',
+            "email"        => 'user1@gmail.com',
             "password"     => Hash::make('password'),
             "company_name" => 'テスト株式会社',
             "role_id"      => Role::getUserId(),
         ]);
         User::create([
-            "name"         => '配送業者',
-            "email"        => 'delivery_agent@gmail.com',
+            "name"         => 'キツネカフェ',
+            "email"        => 'user2@gmail.com',
+            "password"     => Hash::make('password'),
+            "company_name" => 'テスト株式会社',
+            "role_id"      => Role::getUserId(),
+        ]);
+        User::create([
+            "name"         => '佐藤真澄',
+            "email"        => 'delivery_agent1@gmail.com',
+            "password"     => Hash::make('password'),
+            "company_name" => 'テスト株式会社',
+            "role_id"      => Role::getDeliveryAgentId(),
+        ]);
+        User::create([
+            "name"         => '渡辺誠',
+            "email"        => 'delivery_agent2@gmail.com',
+            "password"     => Hash::make('password'),
+            "company_name" => 'テスト株式会社',
+            "role_id"      => Role::getDeliveryAgentId(),
+        ]);
+        User::create([
+            "name"         => '三島海斗',
+            "email"        => 'delivery_agent3@gmail.com',
+            "password"     => Hash::make('password'),
+            "company_name" => 'テスト株式会社',
+            "role_id"      => Role::getDeliveryAgentId(),
+        ]);
+        User::create([
+            "name"         => '鈴木美心',
+            "email"        => 'delivery_agent4@gmail.com',
             "password"     => Hash::make('password'),
             "company_name" => 'テスト株式会社',
             "role_id"      => Role::getDeliveryAgentId(),
