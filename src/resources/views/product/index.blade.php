daf4e1a9ce18913f01354b42a501ed9239b5551e
umiumi2002 <86920962+umiumi2002@users.noreply.github.com>
2023-02-21 23:48:29 +0000

    ユーザートップのタイマーバグ修正

--- src/resources/views/product/index.blade.php
+++ src/resources/views/product/index.blade.php
@@ -124,22 +124,22 @@
                 /*
                 いつまでに注文するといつまでに届くのかを表示する
                     12:00:00:000 ~ 22:59:59:999 の注文の場合は、明日の午前中
-                    03:00:00:000 ~ 11:59:59:999 の注文の場合は、今日の午後
-                    00:00:00:000 ~ 02:59:59:999 の注文の場合は、今日の午前中
+                    23:00:00:000 ~ 23:59:59:999 の注文の場合は、明日の午後
+                    00:00:00:000 ~ 11:59:59:999 の注文の場合は、今日の午後
                 */
                 function showCountdown() {
                     const now = new Date();
                     const nowHour = now.getHours();
                     var targetDate, targetMessage;
 
-                    if (12 <= nowHour && nowHour < 24) {
+                    if (12 <= nowHour && nowHour < 23) {
                         targetDate = (new Date()).setHours(22, 59, 59, 999);
                         targetMessage = "明日の午前中";
-                    } else if (3 <= nowHour && nowHour < 12) {
-                        targetDate = (new Date()).setHours(11, 59, 59, 999);
-                        targetMessage = "今日の午後";
+                    } else if (23 <= nowHour && nowHour < 24) {
+                        targetDate = (new Date()).setHours(23, 59, 59, 999);
+                        targetMessage = "明日の午後";
                     } else {
-                        targetDate = (new Date()).setHours(2, 59, 59, 999);
+                        targetDate = (new Date()).setHours(11, 59, 59, 999);
                         targetMessage = "今日の午前中";
                     }
 
