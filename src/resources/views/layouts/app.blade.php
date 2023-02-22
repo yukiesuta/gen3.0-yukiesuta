c4556714366789a74193658388e7d262925f7477
asaka0408 <sakanadaisuki0408@softbank.ne.jp>
2023-02-23 04:55:51 +0900

    headerのナビゲーション表示・非表示の条件分岐

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -35,9 +35,11 @@
                             <a href="{{  route('order') }}" class="nav-link"><i class="material-icons md-light cartColor">receipt</i>注文履歴</a>
                         </li>
                         @endif
+                        @if ('admin' !== session()->get('role') && 'delivery-agent' !== session()->get('role'))
                         <li class="nav-item">
                             <a href="{{  route('cart') }}" class="nav-link"><i class="material-icons md-light cartColor">shopping_cart</i>カートに行く</a>
                         </li>
+                        @endif
                         @auth
                             <li class="nav-item">
                                 <a href="{{  route('logout') }}" class="nav-link"><i class="material-icons md-light cartColor">exit_to_app</i>ログアウト</a>
